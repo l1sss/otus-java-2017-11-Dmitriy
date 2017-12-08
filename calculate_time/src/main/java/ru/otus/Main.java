9bd9f5cc01bb12b4e0e4298f9823403cdcbe0d02
Dima Slisenko <dm.slisenko@gmail.com>
2017-12-08 11:10:35 +0300

    simple collection time benchmark

--- calculate_time/src/main/java/ru/otus/Main.java
+++ calculate_time/src/main/java/ru/otus/Main.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Main {
-    private static final int MEASURE_COUNT = 100;
+    private static final int MEASURE_COUNT = 1000;
+    private static final int WARMUP_COUNT = 1000;
 
     public static void main(String[] args) {
         Collection<Integer> example = new ArrayList<>();
@@ -14,10 +15,13 @@ public class Main {
             example.add(i);
         }
 
-        calcTime(() -> example.contains(500_000));
+        calcTime(() -> example.contains(9_999_999));
     }
 
     private static void calcTime(Runnable runnable) {
+        for (int i = 0; i < WARMUP_COUNT; i++)
+            runnable.run();
+
         long startTime = System.nanoTime();
 
         for (int i = 0; i < MEASURE_COUNT; i++)
